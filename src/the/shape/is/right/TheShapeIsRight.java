d6239a3f553d932df626b1ba9d95f0c7e19113a3
Ben Leavitt <vtleavs@gmail.com>
2016-11-03 14:31:42 -0400

    Created separate pane views with buttons to switch between them

--- src/the/shape/is/right/TheShapeIsRight.java
+++ src/the/shape/is/right/TheShapeIsRight.java
@@ -10,41 +10,67 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  *
  * @author Ben
  */
-public class TheShapeIsRight extends Application {
-    
+public class TheShapeIsRight extends Application 
+{
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) 
+    {
+        Button toGameButton = new Button("Go To Game Pane");
+        Button toResultsButton = new Button("Go To Results Pane");
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox optionsPane = new VBox();
+        VBox gamePane = new VBox();
+        VBox resultsPane = new VBox();
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(optionsPane, 600, 600);
+        
+        // Add nodes to optionsPane
+        optionsPane.getChildren().add(new Label("This is the Options Pane"));
+        optionsPane.getChildren().add(toGameButton);
+        
+        // Add nodes to gamePane
+        gamePane.getChildren().add(new Label("This is the Game Pane"));
+        gamePane.getChildren().add(toResultsButton);
+        
+        // Add nodes to resultsPane
+        resultsPane.getChildren().add(new Label("This is the Results Pane"));
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        
+        toGameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                scene.setRoot(gamePane);
+            }
+        });
+        
+        toResultsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                scene.setRoot(resultsPane);
+            }
+        });
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch(args);
     }
     
